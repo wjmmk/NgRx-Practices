e62a412b7d8e59c6193b893a096c7d70437d18b4
William Mosquera <wjmmk3@gmail.com>
2023-02-25 07:08:50 -0500

    Implementation of todos Module

---
+++ src/app/todos/model/initial-todos.ts
@@ -0,0 +1,24 @@
+import { Todo } from './todo.interface';
+
+export const initialTodos: Todo[] = [
+  {
+    id: 'c407874580eal',
+    description: 'Escribir escaleta',
+    completed: false,
+  },
+  {
+    id: 'e1495410c519a6',
+    description: 'Grabar Audio',
+    completed: false,
+  },
+  {
+    id: 'e2505508620180',
+    description: 'Crear Clips',
+    completed: false,
+  },
+  {
+    id:'w2457j3643238k' ,
+    description: 'Programar hasta lograr el Éxito',
+    completed: false,
+  }
+];
