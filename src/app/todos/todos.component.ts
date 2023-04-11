2989b3b4d6889f80a514e17e47cf348ae9700278
William Mosquera <wjmmk3@gmail.com>
2023-04-11 17:06:26 -0500

    todos-Component Refactored

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { createTodo, initialTodos, Todo } from './model';
-import { TodosPageActions } from './state';
+import { TodosPageActions, TodosSelectors } from './state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ako-todos',
@@ -10,46 +11,44 @@ import { TodosPageActions } from './state';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodosComponent implements OnInit {
-  todos: Todo[] = [];
+  todos$: Observable<Todo[]> = this.store.select(TodosSelectors.todos);
 
-  get hasCompletedTodos(): boolean {
-    return this.todos.some((todo) => todo.completed);
-  }
+  hasCompletedTodos$: Observable<boolean> = this.store.select(TodosSelectors.hasCompletedTodos);
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
     this.store.dispatch(TodosPageActions.init());
-    this.todos = initialTodos;
+    //this.todos = initialTodos;
   }
 
   addTodo(description: string): void {
     const newTodo = createTodo(description);
     this.store.dispatch(TodosPageActions.addTodo({ todo: newTodo }));
-    this.todos = [...this.todos, newTodo];
+    //this.todos = [...this.todos, newTodo];
   }
 
   removeTodo(todoToRemove: Todo): void {
     this.store.dispatch(TodosPageActions.removeTodo({ todo: todoToRemove }));
-    this.todos = this.todos.filter((todo) => todo.id !== todoToRemove.id);
+    //this.todos = this.todos.filter((todo) => todo.id !== todoToRemove.id);
   }
 
   markAsCompleted(todoToMark: Todo): void {
     this.store.dispatch(TodosPageActions.markAsCompleted({ todo: todoToMark }));
-    this.todos = this.todos.map((todo) =>
+    /* this.todos = this.todos.map((todo) =>
       todo.id === todoToMark.id ? { ...todo, completed: true } : todo
-    );
+    ); */
   }
 
   markAsPending(todoToMark: Todo): void {
     this.store.dispatch(TodosPageActions.markAsPending({ todo: todoToMark}));
-    this.todos = this.todos.map((todo) =>
+    /* this.todos = this.todos.map((todo) =>
       todo.id === todoToMark.id ? { ...todo, completed: false } : todo
-    );
+    ); */
   }
 
   clearCompleted(): void {
     this.store.dispatch(TodosPageActions.clearCompleted());
-    this.todos = this.todos.filter((todo) => todo.completed === false);
+    //this.todos = this.todos.filter((todo) => todo.completed === false);
   }
 }
